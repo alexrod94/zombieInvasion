ecb9fe962d1ee65da9aa33dee7926e85bd9e6101
Alex Rodríguez <alexrod94@gmail.com>
2019-10-16 17:49:28 +0200

    Primera versión

---
+++ comparacion.js
@@ -0,0 +1,10 @@
+let infectar = (contagio, temperatura) => {
+    if (contagio === 2 && temperatura === 3) {
+        probalidad = 90%
+        if (infeccion === true) {
+            porcentaje_zombies++
+        } else if (infeccion === false) {
+            baja la probabilidad
+        }
+    }
+}
